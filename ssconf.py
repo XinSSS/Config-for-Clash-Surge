14ba8d1469221dad02ded5df2221cc3d28fc3ec4
XinSSS <xinreceive@outlook.com>
2019-05-20 10:31:58 +0800

    Quan添加SpeedTest和Spotify规则

--- ssconf.py
+++ ssconf.py
@@ -70,9 +70,12 @@ def getGfwList():
                 try:
                     found = domainList.index(domain[0])
                 except ValueError:
-                    domainList.append(domain[0])
-                    gfwListTxt.write('DOMAIN-SUFFIX,%s,Proxy\n' % (domain[0]))
-                    gfwSurgeRULESETFile.write('DOMAIN-SUFFIX,%s\n' % (domain[0]))
+                    if "spotify.com" in domain[0]:
+                        continue
+                    else:
+                        domainList.append(domain[0])
+                        gfwListTxt.write('DOMAIN-SUFFIX,%s,Proxy\n' % (domain[0]))
+                        gfwSurgeRULESETFile.write('DOMAIN-SUFFIX,%s\n' % (domain[0]))
             else:
                 continue
 
